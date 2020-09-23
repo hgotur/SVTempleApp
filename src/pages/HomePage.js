de7076caa4fb4426543bb63ec445b96e3264fcb7
Harsha Gotur <hgotur@gmail.com>
2020-09-22 22:23:45 -0400

    remove unused

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { View, FlatList, ActivityIndicator } from 'react-native';
+import { View, FlatList } from 'react-native';
 import { connect } from 'react-redux';
 
 import { getPagesMetadata } from '../redux/PageSlice';
